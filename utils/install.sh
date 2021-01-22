9f766fc63cbb8cac586b92f16b0ffa7fc20b1e1f
sp1thas <sp1thas@autistici.org>
2021-01-22 22:51:55 +0200

    [fix]: installation script

--- utils/install.sh
+++ utils/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf /usr/local/bin/dropboxignore
-wget -q https://raw.githubusercontent.com/sp1thas/dropboxignore/master/dropboxignore.sh -O /usr/local/bin/dropboxignore
+wget -q https://raw.githubusercontent.com/sp1thas/dropboxignore/master/bin/dropboxignore.sh -O /usr/local/bin/dropboxignore
 chmod +x /usr/local/bin/dropboxignore
 dropboxignore version
