78ed66bc3258a88dbdf2290ceb3bb763b4672880
drallas <drallas@icloud.com>
2021-01-30 00:32:26 +0100

    without cd.

--- utils/install.sh
+++ utils/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 rm -rf /usr/local/bin/dropboxignore/*
 git clone https://github.com/sp1thas/dropboxignore.git
-cd dropboxignore/
-sudo make install
-cd .. 
+sudo make dropboxignore/install
 rm -rf dropboxignore/
