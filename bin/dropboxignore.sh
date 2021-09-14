27246b7b5e1aa4fe1ea51c43cf34f3f2fe94e82e
Panagiotis Simakis <sp1thas@autistici.org>
2021-09-14 17:45:57 +0300

    [fix] issue when trying to ignore a signle folder without using a dropboxignore file

--- bin/dropboxignore.sh
+++ bin/dropboxignore.sh
@@ -6,7 +6,7 @@
 IFS='
 '
 set -f
-VERSION=v1.0.0
+VERSION=v1.0.1
 DROPBOX_IGNORE_FILE_NAME=".dropboxignore"
 GIT_IGNORE_FILE_NAME=".gitignore"
 MACHINE="$(uname -s)"
@@ -418,30 +418,34 @@ ignore_files() {
     ignore_file "$1"
   elif [ -d "$1" ]; then
     find_dropboxignore_files "$1"
-    for dropboxignore_file in $DROPBOX_IGNORE_FILES; do
-      file_total_results=0
-      # shellcheck disable=SC2013
-      if grep -q -P '^\s*!' "$dropboxignore_file"; then
-        echo -e "$YELLOW$(get_relative_path "$dropboxignore_file" "$BASE_FOLDER") contains exception patterns, will be ignored"
-        continue
-      fi
-      while read -r file_pattern; do
-        file_pattern=${file_pattern%/}
-        subdir="$(dirname "$file_pattern")"
-        pattern="$(basename "$file_pattern")"
-        n_results=0
-        while read -r file_path; do
-          ignore_file "$file_path"
-          ((n_results++))
-        done < <(find "$(dirname "$dropboxignore_file")/$subdir" -name "$pattern")
-        file_total_results=$((file_total_results + n_results))
-      done < <(grep -v -P '^\s*$|^\s*\#|^\s*!' "$dropboxignore_file")
-      log_debug "Matched files because of '$(get_relative_path "$dropboxignore_file" "$BASE_FOLDER")': $file_total_results"
-    done
-    echo -e "$BLUE
+    if [ -z "$DROPBOX_IGNORE_FILES" ]; then
+      ignore_file "$1"
+    else
+        for dropboxignore_file in $DROPBOX_IGNORE_FILES; do
+          file_total_results=0
+          # shellcheck disable=SC2013
+          if grep -q -P '^\s*!' "$dropboxignore_file"; then
+            echo -e "$YELLOW$(get_relative_path "$dropboxignore_file" "$BASE_FOLDER") contains exception patterns, will be ignored"
+            continue
+          fi
+          while read -r file_pattern; do
+            file_pattern=${file_pattern%/}
+            subdir="$(dirname "$file_pattern")"
+            pattern="$(basename "$file_pattern")"
+            n_results=0
+            while read -r file_path; do
+              ignore_file "$file_path"
+              ((n_results++))
+            done < <(find "$(dirname "$dropboxignore_file")/$subdir" -name "$pattern")
+            file_total_results=$((file_total_results + n_results))
+          done < <(grep -v -P '^\s*$|^\s*\#|^\s*!' "$dropboxignore_file")
+          log_debug "Matched files because of '$(get_relative_path "$dropboxignore_file" "$BASE_FOLDER")': $file_total_results"
+        done
+    fi
+  fi
+  echo -e "$BLUE
 Total number of ignored files: $TOTAL_N_IGNORED_FILES $DEFAULT
 Total number of ignored folders: $TOTAL_N_IGNORED_FOLDERS $DEFAULT"
-  fi
 }
 
 #######################################
