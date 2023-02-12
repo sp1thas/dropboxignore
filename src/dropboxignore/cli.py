6d685ffc092e2cdb1baaad6165bc83ec6d0420bb
sp1thas <sp1thas@autistici.org>
2023-02-12 23:16:57 +0200

    implement commands

--- src/dropboxignore/cli.py
+++ src/dropboxignore/cli.py
@@ -1,44 +1,52 @@
-from functools import partial
+import typer
+import os
 
-import fire
-
-from dropboxignore import NAME
-
-
-class Cli:
-    def ignore(self, path: str = ".", v: int = 1):
-        """Ignore file or folder from dropbox under the given path.
-
-        :param v: Whatever
-        :param path: name of the user
-        """
-        pass
-
-    def generate(self, path: str = ".", v: int = 1):
-        """Generate .dropboxignore files based on existing .gitignore files."""
-        pass
-
-    def list(self, path: str = ".", v: int = 1):
-        """List ignored files and folders under the given path."""
-        pass
-
-    def delete(self, path: str = ".", v: int = 1):
-        """Delete specific .dropboxignore file or every .dropboxignore file under the given path."""
-        pass
-
-    def update(self, path: str = ".", v: int = 1):
-        """Update existing .dropboxignore files if at least one .gitignore file has been changed."""
-
-    def genupi(self, path: str = ".", v: int = 1):
-        """Generate, update & ignore using one shortcut command."""
-        pass
-
-    def revert(self, path: str = ".", v: int = 1):
-        """Revert ignored file or folder under the given path."""
-        pass
+# from src.commands.delete import DeleteCommand
 
 
-_cli_partial = partial(fire.Fire, Cli, name=NAME)
+app = typer.Typer(help="Awesome CLI user manager.")
+
+default_path = os.getcwd()
+
+
+@app.command()
+def ignore(path: str = "."):
+    pass
+
+
+@app.command()
+def generate(path: str = "."):
+    pass
+
+
+@app.command()
+def list(path: str = "."):
+    pass
+
+
+@app.command()
+def delete(path: str):
+    from dropboxignore.commands.delete import DeleteCommand
+
+    cmd = DeleteCommand(path)
+    cmd.run()
+    cmd.run_report()
+
+
+@app.command()
+def update(path: str = "."):
+    pass
+
+
+@app.command()
+def genupi(path: str = "."):
+    pass
+
+
+@app.command()
+def revert(path: str = "."):
+    pass
+
 
 if __name__ == "__main__":
-    _cli_partial()
+    app()
