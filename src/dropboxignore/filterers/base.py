1fc1e60a25705b9787904100b3a6973239f4b4cf
sp1thas <sp1thas@autistici.org>
2023-03-05 04:05:22 +0200

    fix joinpath

--- src/dropboxignore/filterers/base.py
+++ src/dropboxignore/filterers/base.py
@@ -1,11 +1,10 @@
 from abc import abstractmethod
 from pathlib import Path
-from typing import Union
 
 
 class BaseFilterer:
-    def __init__(self, path: Union[str, Path]):
-        self.path = Path(path) if isinstance(path, str) else path
+    def __init__(self, path: Path):
+        self.path = path
 
     @abstractmethod
     def __iter__(self):
--- tests/integration/commands/test_delete.py
+++ tests/integration/commands/test_delete.py
@@ -10,7 +10,7 @@ def test_delete_success(tmp_path: Path, capfd: CaptureFixture) -> None:
     di = tmp_path.joinpath(IgnoreFile.DROPBOXIGNORE.value)
     di.touch()
 
-    cli(command=f"--path {tmp_path} delete")
+    cli(command=f"--path '{tmp_path}' delete")
 
     out, err = capfd.readouterr()
     assert f"Number of deleted files: 1 | matched: 1\n" in out
