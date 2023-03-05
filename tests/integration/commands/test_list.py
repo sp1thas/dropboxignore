571551ba3ee35ceaa66b4210c9aa55578fddde26
sp1thas <sp1thas@autistici.org>
2023-03-05 03:22:39 +0200

    remove absolute

--- tests/integration/commands/test_list.py
+++ tests/integration/commands/test_list.py
@@ -4,4 +4,4 @@ from dropboxignore.cli import cli_partial as cli
 
 
 def test_list_success(tmp_path: Path) -> None:
-    cli(command=f"--path {tmp_path.absolute()} list")
+    cli(command=f"--path {tmp_path} list")
