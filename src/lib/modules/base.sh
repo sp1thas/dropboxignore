caf09b96269eb213b27d5a236d46230660092055
sp1thas <sp1thas@autistici.org>
2023-10-17 21:26:48 +0300

    fix realpath command

--- src/lib/modules/base.sh
+++ src/lib/modules/base.sh
@@ -16,4 +16,5 @@ TOTAL_N_GENERATED_FILES=0
 BASE_FOLDER="$PWD"
 FILE_ATTR_NAME="com.dropbox.ignored"
 [[ $MACHINE == Darwin ]] && GREP_CMD="ggrep" || GREP_CMD="grep"
+[[ $MACHINE == Darwin ]] && REALPATH_CMD="$(brew --prefix)/bin/realpath" || GREP_CMD="realpath"
 [[ $MACHINE == Darwin ]] && DIFF_CMD="$(brew --prefix)/bin/diff" || DIFF_CMD="diff"
